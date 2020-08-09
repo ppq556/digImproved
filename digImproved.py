b937f0e77bc6eb54e80df03e33793c9846ffe5e9
ppqa <malloc@linuxpl.net>
2020-08-09 14:38:42 +0200

    digImproved.py: poprawienie formatowania i dodanie komentarzy

--- digImproved.py
+++ digImproved.py
@@ -38,30 +38,50 @@ class Entry:
 		self.hotOrNot = ""
 
 	def __str__(self):
+		# jeśli wpis nie ma autora, pomijamy go
 		if self.author == "":
 			return ""
 
-		if re.match(re.compile(blacklisted_leads, flags=re.IGNORECASE), self.lead) != None or re.match(re.compile(blacklisted_text, flags=re.IGNORECASE), self.text) != None or re.match(re.compile(blacklisted_sites, flags=re.IGNORECASE), self.extSite) != None or re.match(re.compile(blacklisted_authors, flags=re.IGNORECASE), self.author) != None:
+		# jeśli lead / text / site zewnętrzny / author / tag pasuje do filtrów, pomijamy go
+		if re.match(re.compile(blacklisted_leads, flags=re.IGNORECASE), self.lead) != None:
                         return ""
-
+		if re.match(re.compile(blacklisted_text, flags=re.IGNORECASE), self.text) != None:
+                        return ""
+		if re.match(re.compile(blacklisted_sites, flags=re.IGNORECASE), self.extSite) != None:
+                        return ""
+		if re.match(re.compile(blacklisted_authors, flags=re.IGNORECASE), self.author) != None:
+                        return ""
 		for tag in self.tags:
 			if re.match(re.compile(blacklisted_tags, flags=re.IGNORECASE), tag) != None:
 				return ""
 
+		# pozostałe wpisy ubieramy w odpowiedni HTML i zwracamy
 		HTMLOutput  = '<li class="link iC ' + self.hotOrNot + '">\n\t<div class="article  clearfix preview   dC" data-type="link" data-id="' + self.eid + '">\n\t\t'
 
+		# czy wpis jest "gorący"?
 		if self.hotOrNot == hotModifier:
 			HTMLOutput += '<i class="icon hot"></i>\n\t\t'
 
-		HTMLOutput += '<div class="diggbox ">\n\t\t\t<a href="#' + self.eid + '" data-ajaxurl="" title="wykop to znalezisko" class="ajax">\n\t\t\t\t<span>' + self.upvotes + '</span>\n\t\t\t</a>\n\t\t</div>\n\n\n\n\n\t\t<div class="media-content m-reset-float ">\n\t\t\t<a href="' + self.intUrl + '" rel="nofollow noopener noreferrer" title="" >\n\t\t\t\t<img src="' + self.imgPrevUrl + '"  alt="' + self.imgPrevAlt + '">\n\t\t\t</a>\n\t\t</div>\n\n\t\t<div class="lcontrast m-reset-margin">\n\t\t\t<h2>\n\t\t\t\t<a href="' + self.intUrl + '" rel="nofollow noopener noreferrer" title="' + self.lead + '" >' + self.imgPrevAlt + '</a>\n\t\t\t</h2>\n\n\t\t\t<div class="fix-tagline">\n\t\t\t\t<a class="color-2 affect" href="https://www.wykop.pl/ludzie/' + self.author + '/"><em>@</em>' + self.author + '</a>\n\n\t\t\t\t<span class="tag create">\n\t\t\t\t\t<a class="affect" href="' + self.extUrl + '" title="Otwórz źródło znaleziska" rel="nofollow noopener" target="_blank">' + self.extSite + '</a>\n\t\t\t\t</span>\n\n\t\t\t\t<span class="tag create">\n\t\t\t\t\t<a class="affect"  href="https://www.wykop.pl/domena/' + self.extSite + '/" title="Zobacz inne znaleziska z tej domeny" />\n\t\t\t\t</span>\n\n'
+		HTMLOutput += '<div class="diggbox ">\n\t\t\t<a href="#' + self.eid + '" data-ajaxurl="" title="wykop to znalezisko" class="ajax">\n\t\t\t\t<span>' + self.upvotes
+		HTMLOutput += '</span>\n\t\t\t</a>\n\t\t</div>\n\n\n\n\n\t\t<div class="media-content m-reset-float ">\n\t\t\t<a href="' + self.intUrl + '" rel="nofollow noopener noreferrer" title="" >\n\t\t\t\t<img src="'
+		HTMLOutput += self.imgPrevUrl + '"  alt="' + self.imgPrevAlt + '">\n\t\t\t</a>\n\t\t</div>\n\n\t\t<div class="lcontrast m-reset-margin">\n\t\t\t<h2>\n\t\t\t\t<a href="' + self.intUrl
+		HTMLOutput += '" rel="nofollow noopener noreferrer" title="' + self.lead + '" >' + self.imgPrevAlt + '</a>\n\t\t\t</h2>\n\n\t\t\t<div class="fix-tagline">\n\t\t\t\t<a class="color-2 affect" href="https://www.wykop.pl/ludzie/'
+		HTMLOutput += self.author + '/"><em>@</em>' + self.author + '</a>\n\n\t\t\t\t<span class="tag create">\n\t\t\t\t\t<a class="affect" href="' + self.extUrl
+		HTMLOutput += '" title="Otwórz źródło znaleziska" rel="nofollow noopener" target="_blank">' + self.extSite + '</a>\n\t\t\t\t</span>\n\n\t\t\t\t<span class="tag create">\n\t\t\t\t\t<a class="affect"  href="https://www.wykop.pl/domena/'
+		HTMLOutput += self.extSite + '/" title="Zobacz inne znaleziska z tej domeny" />\n\t\t\t\t</span>\n\n'
 
+		# tagi dodajemy w specjalny sposób
 		for Tag in self.tags:
 			HTMLOutput += '\t\t\t\t<a class="tag affect create " href="https://www.wykop.pl/tag/' + Tag + '/"><em>#</em>' + Tag + '</a>\n'
 
-		HTMLOutput += '\t\t\t</div>\n\n\t\t\t<div class="description">\n\t\t\t\t<p class="text">\n\t\t\t\t\t<a href="' + self.intUrl + '" title="">\n\t\t\t\t\t' + self.text + '\n\t\t\t\t\t</a>\n\t\t\t\t</p>\n\t\t\t</div>\n\n\t\t\t<div class="row elements">\n\t\t\t\t<a class="affect" href="' + self.intUrl + '">' + self.comments + '</a>\n\n\t\t\t\t<span class="affect">opublikowany \n\t\t\t\t\t<time title="' + self.dateSimple + '" datetime="' + self.dateExtend + '" itemprop="datePublished">' + self.dateMinsAgo + '</time>\n\t\t\t\t</span>\n\n\t\t\t</div>\n\t\t</div>\n\t</div>\n</li>'
+		HTMLOutput += '\t\t\t</div>\n\n\t\t\t<div class="description">\n\t\t\t\t<p class="text">\n\t\t\t\t\t<a href="' + self.intUrl + '" title="">\n\t\t\t\t\t' + self.text
+		HTMLOutput += '\n\t\t\t\t\t</a>\n\t\t\t\t</p>\n\t\t\t</div>\n\n\t\t\t<div class="row elements">\n\t\t\t\t<a class="affect" href="' + self.intUrl + '">' + self.comments
+		HTMLOutput += '</a>\n\n\t\t\t\t<span class="affect">opublikowany \n\t\t\t\t\t<time title="' + self.dateSimple + '" datetime="' + self.dateExtend + '" itemprop="datePublished">'
+		HTMLOutput += self.dateMinsAgo + '</time>\n\t\t\t\t</span>\n\n\t\t\t</div>\n\t\t</div>\n\t</div>\n</li>'
 
 		return HTMLOutput
 
+# Zmienna przechowująca wszystkie obiekty znalezisk, inicjalnie pusta
 EntriesList = []
 
 def printEntries():
